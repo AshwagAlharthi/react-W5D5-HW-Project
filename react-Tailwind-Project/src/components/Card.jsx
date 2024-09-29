53db02ed0fc378bc8349e189e6b63839857643f6
AshwagAlharthi <Ashwag.alharthi@hotmail.com>
2024-09-29 07:48:09 +0300

    solved

--- react-Tailwind-Project/src/components/Card.jsx
+++ react-Tailwind-Project/src/components/Card.jsx
@@ -11,7 +11,7 @@ function Card(props) {
 
                 {
                     props.link &&
-                    <p className="font-montserrat w-[9vw] max-sm:w-[23vw] flex justify-between text-[#c99484] font-bold text-[0.8rem] group hover:text-black">READ MORE <svg className="w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
+                    <p className="font-montserrat w-[9vw] max-sm:w-[26vw] flex justify-between text-[#c99484] font-bold text-[0.8rem] group hover:text-black">READ MORE <svg className="w-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 2048 2048">
                         <path d="M6.125 1088h1797.89l-402.976 403 89.994 90L2048 1024l-556.966-557-89.994 90 402.976 403H6.125v128z"></path>
                     </svg>
                     </p>
